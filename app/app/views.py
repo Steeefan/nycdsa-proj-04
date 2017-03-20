e2c0699e25623f39c6f10aacc289dfc05f5266b1
Steeefan <sh@steeefan.de>
2017-03-20 15:57:50 -0400

    dynamic Flask tpl: first try

--- app/app/views.py
+++ app/app/views.py
@@ -1,10 +1,30 @@
 from flask import Flask, flash, redirect, render_template, request
 from app import app
 from random import randint
+import sqlite3 as lite
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    con = lite.connect('D:/capstone-v2.db')
+    cur = con.cursor()
+
+    rows = cur.execute('SELECT DISTINCT developer FROM tblGame ORDER BY developer ASC;')
+    devs = []
+    [devs.append({
+        'developer': row[0]
+    }) for row in rows
+    ]
+
+    rows = cur.execute('SELECT name, link, developer FROM tblGame LIMIT 20')
+    games = []
+    [games.append({
+        'name': row[0],
+        'link': row[1],
+        'developer': row[2]
+    }) for row in rows
+    ]
+
+    return render_template('index.html', games=games, devs=devs)
 
 @app.route("/user/")
 def hello():
