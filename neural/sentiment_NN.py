1664d300d4d47b22dbc5c66acafa3fec68f3d6ba
djepstein87 <djepstein87@gmail.com>
2017-03-27 21:59:19 -0400

    sentiment file

---
+++ neural/sentiment_NN.py
@@ -0,0 +1,18 @@
+import pickle
+import numpy as np
+import os
+os.environ["THEANO_FLAGS"] = "warn.round=False"
+from keras.preprocessing import text
+from keras.models import load_model
+from keras.preprocessing import sequence
+from keras.models import Sequential
+import keras
+import re
+from sent_predict import cleaning_text
+from sent_predict import sentiment_predictor
+
+input = 'I loved the game! It was awesome'
+rating = 3
+
+
+sentiment_predictor(input, rating)
